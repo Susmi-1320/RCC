a4c58f488cf13adc6104a6495972976e286c9f00
Susmi-1320 <139312227+Susmi-1320@users.noreply.github.com>
2023-07-20 19:49:03 +0530

    google maps (#1)
    * ios maps
    
    * fonts and icons

--- ios/RCCHotels/AppDelegate.mm
+++ ios/RCCHotels/AppDelegate.mm
@@ -1,6 +1,9 @@
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
+// #import <React/RCTBridge.h>
+// #import <React/RCTRootView.h>
+// #import <GoogleMaps/GoogleMaps.h> // Import GoogleMaps module
+
 
 @implementation AppDelegate
 
@@ -10,7 +13,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+//  [GMSServices provideAPIKey:@"AIzaSyCOf7LRg_WZGzj95JPs2gieSnYMxMvgynU"];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
