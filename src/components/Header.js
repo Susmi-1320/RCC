c0ddadad46618fec9aa81964d95d6089993c914a
sumi-143 <139312227+Susmi-1320@users.noreply.github.com>
2023-07-20 17:46:36 +0530

    ios maps

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import globalStyles from '../assets/global/globalStyles';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import COLORS from '../consts/colors';
+import {useNavigation} from '@react-navigation/native';
 
 const Header = ({title, onBack}) => {
+  const navigation = useNavigation();
+  const onPressBack = () => {
+    navigation.goBack();
+  };
   return (
     <View style={[globalStyles.header]}>
-      <MaterialIcons name="arrow-left" color={COLORS.orange} size={40} />
+      <Pressable onPress={onBack ? onBack : onPressBack}>
+        <MaterialIcons name="arrow-left" color={COLORS.orange} size={40} />
+      </Pressable>
       <Text>{title}</Text>
     </View>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import globalStyles from '../assets/global/globalStyles';
+import MapView, {Marker} from 'react-native-maps';
+import Header from './Header';
 const Map = () => {
   return (
     <SafeAreaView style={[globalStyles.whiteBg]}>
-      <View style={[globalStyles.header]}>
-        <Text>Map</Text>
-      </View>
+      <Header />
+      <MapView
+        style={{
+          flex: 1,
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        <Marker
+          coordinate={{latitude: 37.78825, longitude: -122.4324}}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      </MapView>
     </SafeAreaView>
   );
 };
